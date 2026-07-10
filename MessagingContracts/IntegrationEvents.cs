771ec7e15dcc66a2e8f66c22d13cd43c956f99eb
Nikolay Lipchev <nikolay.lipchev.02@gmail.com>
2026-07-10 20:53:57 +0300

    feat: add messaging contracts for kafka

---
+++ MessagingContracts/IntegrationEvents.cs
@@ -0,0 +1,37 @@
+﻿using SharedContracts;
+
+namespace MessagingContracts;
+
+public record BookingCreatedIntegrationEvent(
+        Guid MessageId,
+        Guid BookingId,
+        Guid UserId,
+        Guid EventId,
+        DateTime OccurredAt
+);
+
+public record BookingCancelledIntegrationEvent(
+        Guid MessageId,
+        Guid BookingId,
+        Guid UserId,
+        Guid EventId,
+        DateTime OccurredAt
+);
+
+public record EventCreatedIntegrationEvent(
+        Guid MessageId,
+        Guid EventId,
+        string Title,
+        EventCity City,
+        EventCategory Category,
+        DateTime OccurredAt
+);
+
+public record EventUpdatedIntegrationEvent(
+        Guid MessageId,
+        Guid EventId,
+        string Title,
+        EventCity City,
+        EventCategory Category,
+        DateTime OccurredAt
+);
